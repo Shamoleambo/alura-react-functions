b0c6fcbbf47b836a71052cf8652c5c98770fca5d
Tiago Gomes <tidgomes@gmail.com>
2022-02-11 10:21:33 -0300

    material ui installation

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,7 +1,26 @@
+import Button from "@mui/material/Button";
+
 function FormularioCadastro() {
   return (
     <form>
+      <label>Nome</label>
       <input type="text" />
+
+      <label>Sobrenome</label>
+      <input type="text" />
+
+      <label>CPF</label>
+      <input type="text" />
+
+      <label>Promoções</label>
+      <input type="checkbox" />
+
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button variant="contained" type="submit">
+        Cadastrar
+      </Button>
     </form>
   );
 }
