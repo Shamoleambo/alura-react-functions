6a2e85e6e217fde3ac64e6a4ae070e23044c9be4
Tiago Gomes <tidgomes@gmail.com>
2022-02-16 14:14:24 -0300

    function containing switch to render the correct step if the form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
+
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
+import { Typography } from "@mui/material";
+
 function FormularioCadastro({ onSubmitForm, validarCpfForm }) {
-  return (
-    <>
-      <DadosPessoais
-        onSubmitForm={onSubmitForm}
-        validarCpfForm={validarCpfForm}
-      />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapa, setEtapa] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return (
+          <DadosPessoais
+            onSubmitForm={onSubmitForm}
+            validarCpfForm={validarCpfForm}
+          />
+        );
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
+
+  return <>{formularioAtual(etapa)}</>;
 }
 
 export default FormularioCadastro;
