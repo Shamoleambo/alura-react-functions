951c0246c183fefcb28d28d44f0446e125248383
Tiago Gomes <tidgomes@gmail.com>
2022-02-16 10:29:37 -0300

    new components - DadosUsuario and DadosPessoais with some alterations to the structure of the project

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,29 @@
+import { TextField, Button } from "@mui/material";
+
+function DadosUsuario() {
+  return (
+    <form>
+      <TextField
+        id="email"
+        label="email"
+        type="email"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="senha"
+        label="senha"
+        type="password"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <Button type="submit" variant="contained">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default DadosUsuario;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,78 +1,15 @@
-import { useState } from "react";
-import { Button, TextField, FormControlLabel, Switch } from "@mui/material";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ onSubmitForm, validarCpfForm }) {
-  const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [promocoes, setPromocoes] = useState(true);
-  const [novidades, setNovidades] = useState(true);
-  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
-
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSubmitForm({ nome, sobrenome, cpf, promocoes, novidades });
-      }}
-    >
-      <TextField
-        id="nome"
-        label="Nome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
+    <>
+      <DadosPessoais
+        onSubmitForm={onSubmitForm}
+        validarCpfForm={validarCpfForm}
       />
-      <TextField
-        id="sobrenome"
-        label="Sobrenome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        value={sobrenome}
-        onChange={(e) => setSobrenome(e.target.value)}
-      />
-      <TextField
-        id="cpf"
-        label="CPF"
-        variant="outlined"
-        margin="normal"
-        error={!erros.cpf.valido}
-        helperText={erros.cpf.texto}
-        fullWidth
-        onChange={(e) => setCpf(e.target.value)}
-        onBlur={(e) => {
-          setErros(validarCpfForm(e.target.value));
-        }}
-      />
-
-      <FormControlLabel
-        label="Promoções"
-        control={
-          <Switch
-            checked={promocoes}
-            name="promocoes"
-            onChange={(e) => setPromocoes(e.target.checked)}
-          />
-        }
-      />
-      <FormControlLabel
-        label="Novidades"
-        control={
-          <Switch
-            checked={novidades}
-            name="novidades"
-            onClick={() => setNovidades(() => !novidades)}
-          />
-        }
-      />
-
-      <Button variant="contained" type="submit">
-        Cadastrar
-      </Button>
-    </form>
+      <DadosUsuario />
+    </>
   );
 }
 
