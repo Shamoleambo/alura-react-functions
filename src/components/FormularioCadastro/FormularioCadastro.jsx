bc7e172f4cefb71e6cb2a41235214c3587562a62
Tiago Gomes <tidgomes@gmail.com>
2022-02-17 12:04:40 -0300

    new state dados and use of useEffect

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ onSubmit, validarCpfForm }) {
-  let [etapa, setEtapa] = useState(0);
+  const [etapa, setEtapa] = useState(0);
+  const [dados, setDados] = useState({});
+
+  useEffect(() => {
+    console.log(dados);
+  });
 
   const formularios = [
-    <DadosUsuario onSubmit={onSubmit} />,
-    <DadosPessoais validarCpfForm={validarCpfForm} onSubmit={onSubmit} />,
-    <DadosEntrega onSubmit={onSubmit} />,
+    <DadosUsuario onSubmit={controlarDados} />,
+    <DadosPessoais validarCpfForm={validarCpfForm} onSubmit={controlarDados} />,
+    <DadosEntrega onSubmit={controlarDados} />,
   ];
 
+  function controlarDados(dadosColetados) {
+    setDados({ ...dados, ...dadosColetados });
+    controlarEtapa();
+  }
+
   function controlarEtapa() {
-    setEtapa(() => ++etapa);
+    setEtapa(etapa + 1);
   }
 
   return <>{formularios[etapa]}</>;
